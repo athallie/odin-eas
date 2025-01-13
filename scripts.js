b91b96ead818b713e8155b7824f940bc33a45644
Eavelie <athallahnaufalr@gmail.com>
2025-01-13 11:55:09 +0700

    modified hover effect to change bg using random rgb color

--- scripts.js
+++ scripts.js
@@ -7,6 +7,12 @@ for (let i = 1; i < gridSize * gridSize + 1; i++) {
     gridContainer.appendChild(createGrid(i))
 }
 
+gridContainer.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("grid")) {
+        e.target.style.backgroundColor = getRandomRGBColor()
+    }
+})
+
 function createGrid(i) {
     let grid = document.createElement("div")
     grid.classList.add("grid")
@@ -14,5 +20,10 @@ function createGrid(i) {
     grid.style.flexBasis = `calc(100%/${gridSize})`
     grid.style.maxHeight = `calc(100%/${gridSize})`
     return grid
+}
+
+function getRandomRGBColor() {
+    let getRandomNum = () => Math.floor(Math.random() * 256)
+    return `rgb(${getRandomNum()}, ${getRandomNum()}, ${getRandomNum()})`
 }
 
